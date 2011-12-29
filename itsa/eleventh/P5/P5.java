08928185d6da3c097dff152ef06c14608ed23e9b
david <s97113117.stu@gmail.com>
2011-12-30 00:50:56 +0800

    add Problem 5

--- itsa/eleventh/P5/P5.java
+++ itsa/eleventh/P5/P5.java
@@ -2,21 +2,49 @@ import static java.lang.System.out;
 import java.util.Vector;
 import java.io.*;
 
-class MyReader {
+class Formatter {
+	public int[] getTwoNumbers(String str) {
+		String[] strArray = str.split("\\s");
+		return new int[] {Integer.parseInt(strArray[0]), Integer.parseInt(strArray[1])};
+	}
 
+	public int countZeros(String str) {
+		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if ('0' == str.charAt(i)) {
+				count++;
+			}
+		}
+
+		return count;
+	}
 }
 
 public class P5 {
 
 	public static void main (String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Formatter formatter = new Formatter();
 		String str;
+		String output;
 
 		while ((str = br.readLine()) != null) {
 			int times = Integer.parseInt(str);
-			
+			int zeros;
+
 			for (int i = 0; i < times; i++) {
-			
+				zeros = 0;
+				int[] numbers = formatter.getTwoNumbers(br.readLine());
+				output = "";
+				for (int j = numbers[0] + 1; j < numbers[1]; j++) {
+					String word = j + "";
+					zeros += formatter.countZeros(word);
+				}
+
+				output = (0 == zeros) ? "No Solution" : zeros + "";
+				output += (i == (times - 1)) ? "" : "\n";
+
+				out.println(output);
 			}
 
 		}
